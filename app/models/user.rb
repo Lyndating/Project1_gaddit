21be473e30410cb6be0271aa6d07a4f87b358ed4
Ting Luo <shirleyayu@hotmail.com>
2022-04-08 23:10:00 +1000

    Sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    validates :email, :uniqueness => true, :presence => true
+    has_secure_password
     # has_and_belongs_to_many :channel
     has_many :post
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  get 'pages/home'
+  root :to => "pages#home"
+  resources :users, :only =>[:new, :index, :create] 
 end
